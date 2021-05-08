8e4ec904cdef4c38aa9c3da06d3d35d348cf72b5
mintmint95 <khwankamol.mint@gmail.com>
2021-05-09 02:24:34 +0700

    refactor code

--- duplicated-numbers.js
+++ duplicated-numbers.js
@@ -1,30 +1,43 @@
-function findDuplicatedNumber(numbers) {
-  let countNumber = {}
-  let duplicatedNumbers = []
-
-  if (numbers === null || numbers === true || numbers === false || numbers.length === 0) {
-    return false
-  }
-  if (!numbers.length) {
-    return false
-  }
-
-  for (i = 0; i < numbers.length; i++) {
-    if (numbers[i] === null || numbers[i] === true || numbers[i] === false || numbers[i] < 0 || numbers[i] > 99 || typeof numbers[i] !== 'number') {
-      return false
+function validateFormatOfNumbers(numbers) {
+  if (Array.isArray(numbers)) {
+    if (numbers.length > 0 && numbers.every((value) => value >= 1 && value <= 99)) {
+      return true
     }
+    return false
+  }
+  return false
+}
+
+function countDuplicatedNumber(numbers) {
+  let countNumber = {}
+  for (i = 0; i < numbers.length; i++) {
     if (countNumber[numbers[i]]) {
       countNumber[numbers[i]] += 1
     } else {
       countNumber[numbers[i]] = 1
     }
   }
+  return countNumber
+}
+
+function summaryDuplicatedNumber(countNumber) {
+  let duplicatedNumbers = []
   for (const c in countNumber) {
     if (countNumber[c] > 1) {
       duplicatedNumbers.push(c)
     }
   }
   return duplicatedNumbers.join()
+}
+
+function findDuplicatedNumber(numbers) {
+  const isPass = validateFormatOfNumbers(numbers)
+  if (!isPass) {
+    return false
+  }
+
+  let countNumber = countDuplicatedNumber(numbers)
+  return summaryDuplicatedNumber(countNumber)
 }
 
 module.exports = findDuplicatedNumber
--- duplicated-numbers.test.js
+++ duplicated-numbers.test.js
@@ -1,64 +1,90 @@
 const findDuplicatedNumber = require('./duplicated-numbers.js')
 
-test('Case1: Find duplicated number in [2, 4, 88, 2, 78, 88, 34, 88] to equal 2, 88', () => {
-  //Duplicated first and last in array
-  const input = [2, 4, 88, 2, 78, 88, 34, 88]
-  const result = findDuplicatedNumber(input)
-  expect(result).toBe('2,88')
-})
+describe('Test function findDuplicatedNumber', () => {
+  test('Case1: Find duplicated number in [2, 4, 88, 2, 78, 88, 34, 88] to equal 2, 88', () => {
+    //Duplicated first and last in array
+    const input = [2, 4, 88, 2, 78, 88, 34, 88]
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe('2,88')
+  })
 
-test('Case2: Find duplicated number in [2, 4, 4, 4, 78, 4, 34, 88] to equal 4', () => {
-  //Duplicated in center of array
-  const input = [2, 4, 4, 4, 78, 4, 34, 88]
-  const result = findDuplicatedNumber(input)
-  expect(result).toBe('4')
-})
+  test('Case2: Find duplicated number in [2, 4, 4, 4, 78, 4, 34, 88] to equal 4', () => {
+    //Duplicated in center of array
+    const input = [2, 4, 4, 4, 78, 4, 34, 88]
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe('4')
+  })
 
-test('Case3: Input data is an empty array to return false', () => {
-  const input = []
-  const result = findDuplicatedNumber(input)
-  expect(result).toBe(false)
-})
+  test('Case3: Input data is an empty array to return false', () => {
+    const input = []
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
+  })
 
-test('Case4: Input data is null to return false', () => {
-  const input = null
-  const result = findDuplicatedNumber(input)
-  expect(result).toBe(false)
-})
+  test('Case4: Input data is null to return false', () => {
+    const input = null
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
+  })
 
-test('Case5: Input data is true to return false', () => {
-  const input = true
-  const result = findDuplicatedNumber(input)
-  expect(result).toBe(false)
+  test('Case5: Input data is true to return false', () => {
+    const input = true
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
 
-})
+  })
 
-test('Case6: Input data is false to return false', () => {
-  const input = false
-  const result = findDuplicatedNumber(input)
-  expect(result).toBe(false)
-})
+  test('Case6: Input data is false to return false', () => {
+    const input = false
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
+  })
 
-test('Case7: Input data is incorrect (Numbers not match 0 - 99) to return false', () => {
-  const input = [2, 4, 88, 2, 78, 88, 34, 99.99]
-  const result = findDuplicatedNumber(input)
-  expect(result).toBe(false)
-})
+  test('Case7: Input data is incorrect (Numbers not match 0 - 99) to return false', () => {
+    const input = [2, 4, 88, 2, 78, 88, 34, 99.99]
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
+  })
 
-test('Case8: Input data have minus number to return false', () => {
-  const input = [-2.5, -4, 88, 2, 78, 88, 34, 88]
-  const result = findDuplicatedNumber(input)
-  expect(result).toBe(false)
-})
+  test('Case8: Input data have minus number to return false', () => {
+    const input = [-2.5, -4, 88, 2, 78, 88, 34, 88]
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
+  })
 
-test('Case9: Input data have space to return false', () => {
-  const input = ' '
-  const result = findDuplicatedNumber(input)
-  expect(result).toBe(false)
-})
+  test('Case9: Input data have space to return false', () => {
+    const input = ' '
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
+  })
 
-test('Case10: Input data as string to return false', () => {
-  const input = 'test'
-  const result = findDuplicatedNumber(input)
-  expect(result).toBe(false)
+  test('Case10: Input data as string to return false', () => {
+    const input = 'test'
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
+  })
+
+  test('Case11: Input data as 0 to return false', () => {
+    const input = 0
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
+  })
+
+  test('Case12: Input data as 1 to return false', () => {
+    const input = 1
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
+  })
+
+  test('Case13: Input data as 10 to return false', () => {
+    const input = 10
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
+  })
+
+  test('Case14: Input data as undefined to return false', () => {
+    const input = undefined
+    const result = findDuplicatedNumber(input)
+    expect(result).toBe(false)
+  })
 })
